50fd3946ad8bd7a19ccaf4ce8c15ed40ce6cb922
yun <taro.yamada@email.com>
2026-01-08 10:28:55 +0900

    メールアドレスが重複してないか確認する処理を追加

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+    boolean existsByEmail(String email);
 }
